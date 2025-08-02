5db925c5b992e618f1780142784aff56931b4d6b
Matt Lewis <matt@mplewis.com>
2025-08-02 16:06:03 -0600

    feat: set default quality to 70

--- cmd/transcode.go
+++ cmd/transcode.go
@@ -33,7 +33,7 @@ func init() {
 	transcodeCmd.Flags().StringVarP(&transcodeOutputSuffix, "suffix", "s", "-optimized", "Output file suffix")
 	transcodeCmd.Flags().BoolVarP(&transcodeOverwrite, "overwrite", "o", false, "Overwrite existing output files")
 	transcodeCmd.Flags().BoolVarP(&transcodeVerbose, "verbose", "v", false, "Enable verbose logging")
-	transcodeCmd.Flags().IntVarP(&transcodeQuality, "quality", "q", 80, "Video quality (0-100, higher is better quality)")
+	transcodeCmd.Flags().IntVarP(&transcodeQuality, "quality", "q", 70, "Video quality (0-100, higher is better quality)")
 }
 
 func runTranscode(cmd *cobra.Command, args []string) error {
